fe943f0eab3b089c3d68925b085187bb4d7b98a2
Aditya Taksande <taksandeaditya@gmail.com>
2021-04-17 20:26:41 +0530

    updated database

--- cloneamazonapp/models.py
+++ cloneamazonapp/models.py
@@ -51,14 +51,36 @@ class SubCategories(models.Model):
 class Products(models.Model):
     id = models.AutoField(primary_key=True)
     product_name = models.CharField(max_length=255)
+    url_slug = models.CharField(max_length=255)
+    subcategories = models
     brand = models.CharField(max_length=255)
     product_max_price = models.CharField(max_length=255)
     product_discount_price = models.CharField(max_length=255)
     product_description = models.TextField()
     product_long_description = models.TextField()
+    in_stock_total = models.IntegerField(default=1)
     is_active = models.IntegerField(default=1)
     added_by_merchant = models.ForeignKey(MerchantUser,on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+class ProductMedia(models.Model):
+    id = models.AutoField(primary_key=True)
+    product_id = models.ForeignKey(Products, on_delete=models.CASCADE )
+    media_type_choice = ((1,"Image"),(2,"Video"))
+    media_type = models.CharField(max_length=255)
+    media_content = models.FileField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.IntegerField(default=1)
+
+
+class ProductTranssaction(models.Model):
+    id = models.AutoField(primary_key=True)
+    transaction_type_choices = ((1,"BUY"),(2,"SELL"))
+    product_id = models.ForeignKey(Products, on_delete=models.CASCADE )
+    transaction_product_count = models.IntegerField(default=1)
+    transaction_type = models.CharField(choices=transaction_type_choices,max_length=255)
+    trasaction_description = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True) 
 
 class ProductDetails(models.CharField):
     id = models.AutoField(primary_key=True)
@@ -82,4 +104,57 @@ class ProductTags(models.Model):
     title = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     is_active = models.IntegerField(default=1)
+
+class ProductQuestions(models.Model):
+    id = models.AutoField(primary_key=True)
+    product_id = models.ForeignKey(Products, on_delete=models.CASCADE )
+    question = models.TextField()
+    answer = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.IntegerField(default=1)
+
+class ProductReviews(models.Model):
+    id = models.AutoField(primary_key=True)
+    product_id = models.ForeignKey(Products, on_delete=models.CASCADE )
+    user_id = models.ForeignKey(CustomerUser,on_delete=models.CASCADE)
+    rating = models.CharField(default="5")
+    review = models.TextField(default="")
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.IntegerField(default=1)
+
+
+class ProductReviewVoting(models.Model):
+    id = models.AutoField(primary_key=True)
+    product_review_id = models.ForeignKey(ProductReviews, on_delete=models.CASCADE )
+    user_id_voting = models.ForeignKey(CustomerUser,on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.IntegerField(default=1)
+
+
+class ProductVarient(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class ProductVarientItems(models.Model):
+    id = models.AutoField(primary_key=True)
+    product_id = models.ForeignKey(Products,on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CustomerOrders(models.Model):
+    id = models.AutoField(primary_key=True)
+    product_id = models.ForeignKey(Products,on_delete=models.DO_NOTHING)
+    purchase_price = models.CharField(max_length=255)
+    coupon_code = models.CharField(max_length=255)
+    discount_amt = models.CharField(max_length=255)
+    product_status = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class OrderDeliveryStatus(models.Model):
+    id = models.AutoField(primary_key=True)
+    order_id = models.ForeignKey(CustomerOrders,on_delete=models.CASCADE)
+    status = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
