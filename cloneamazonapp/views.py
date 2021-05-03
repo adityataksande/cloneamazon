b14969cc99a48f6a293be02ffc08600a7d22a303
Aditya Taksande <taksandeaditya@gmail.com>
2021-05-03 14:50:28 +0530

    added static to the app

--- cloneamazonapp/views.py
+++ cloneamazonapp/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 # Create your views here.
 def demoPage(requset):
     return HttpResponse("Demo")
+
+def demoPageTemplate(requset):
+    return render(requset, "demo.html")
